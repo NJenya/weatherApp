76b23f0220b9ac277a293146ebed433270530da2
Jenya Nemyrovskiy <njenya@gmail.com>
2019-01-05 12:00:28 +0200

    lesson 2

---
+++ src/components/info.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Info extends React.Component {
+	render(){
+		return (
+			<div>
+				<h2>Weather App</h2>
+				<p>Find out the Weather in your City</p>
+			</div>
+		);	
+	}
+}
+
+export default Info;
